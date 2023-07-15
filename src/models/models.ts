3400555c72591ea11baf6bb5a5e1bc10d97e1ba9
Brian <xxblyxx@gmail.com>
2023-07-15 15:00:59 -0700

    fix-missing quotes

--- src/models/models.ts
+++ src/models/models.ts
@@ -103,7 +103,7 @@ export const MODELS: Record<string, ModelDefinition[]> = {
   "roborock.vacuum.a75": [
     {
       speed: speedmodes["gen5"],
-      waterspeed: watermodes["gen1+customLessMop],
+      waterspeed: watermodes["gen1+customLessMop"],
     },
   ],
   // S8
